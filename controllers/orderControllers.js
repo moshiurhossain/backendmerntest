bbb66fdeaf922b04152a05fffe0b263063d89d68
unknown <pantho883@gmail.com>
2026-08-03 20:06:34 +0600

    order function added

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -1,5 +1,7 @@
 const { apiResponse } = require("../utilities/apiResponse")
 const { asyncHandler } = require("../utilities/asyncHandler")
+const cartSchema = require("../model/cart.model")
+const orderSchema = require("../model/order.model")
 
 const addOrder = asyncHandler(async (req, res, next) => {
     
@@ -7,7 +9,28 @@ const addOrder = asyncHandler(async (req, res, next) => {
     if(!user || !phone || !address) return apiResponse(res,404,'You must enter all information')
 
     const cartdata = await cartSchema.find({user})
-    apiResponse(res, 200, "Order added successfully")
+
+    const items = cartdata.map(item => {
+        return {
+            foodId:item.foodId,
+            quantity:item.quantity,
+            totalPrice:item.totalPrice,
+        }   
+    })
+
+    const totalCartItemPrice = cartdata.reduce((acc, item) => acc + item.totalPrice, 0)
+
+    const order = new orderSchema({
+        user,
+        phone,
+        address,
+        items,
+        totalItemPrice:totalCartItemPrice,
+    })
+    await order.save()
+    
+
+    apiResponse(res, 200, "Order added successfully",cartdata)
 })
 
 module.exports = { addOrder }
